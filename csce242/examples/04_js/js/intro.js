a2e4eefd9084f45ae6bf5a29524c231f0107e482
Ash5543 <ashpaige322@gmail.com>
2025-09-11 16:17:26 -0400

    added javascript example

--- csce242/examples/04_js/js/intro.js
+++ csce242/examples/04_js/js/intro.js
@@ -6,6 +6,31 @@ const sayHello = () =>{
 document.getElementById("btn-click-me").onclick = sayHello;  parentheses calls the function on refresh, const for variables
 */
 
-document.getElementById("btn-click-me").onclick = () => {
-    console.log("hello world");
+document.getElementById("btn-click-me").onclick = (event) => {
+    document.getElementById("p-welcome").innerHTML="Hello World";
+    event.currentTarget.classList.add("clicked"); //currentTarget is the button that was clicked
 };
+
+document.getElementById("happy").onclick = (event) => {
+    document.getElementById("paragraph").innerHTML="yay";
+    event.currentTarget.classList.add("happyclicked");
+};
+document.getElementById("sad").onclick = (event) => {
+    document.getElementById("paragraph").innerHTML="nay";
+    event.currentTarget.classList.add("sadclicked");
+};
+document.getElementById("clear").onclick = (event) => {
+    document.getElementById("paragraph").innerHTML="";
+    document.getElementById("happy").classList.remove("happyclicked");
+    document.getElementById("sad").classList.remove("sadclicked");
+};
+
+//input type = color
+
+document.getElementById("txt-emotion").onkeyup = (event) =>{
+    const userInput = event.currentTarget.value;
+    document.getElementById("p-emotion").innerHTML=`You are feeling ${userInput}`;
+    document.getElementById("img-emotion").classList.remove("hidden");
+};
+
+//.style.color for color picker
