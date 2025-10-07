c6431bf9f4ade9f7f4fb5832098ca77b3a1e27f9
Ash5543 <ashpaige322@gmail.com>
2025-10-07 14:15:32 -0400

    updated project part 6

--- csce242/projects/part6/js/json.js
+++ csce242/projects/part6/js/json.js
@@ -1,5 +1,4 @@
 const getSandwiches=async()=>{
-
     const url = "https://ash5543.github.io/csce242/projects/part6/json/sandwiches.json"
     try{
         const response = await fetch(url);
@@ -12,11 +11,28 @@ const getSandwiches=async()=>{
 
 const showSandwiches=async()=>{
     const sandwiches = await getSandwiches();
-    console.log(sandwiches);
+    const hotmenu = document.getElementById("hot-menu");
+    const coldmenu = document.getElementById("cold-menu");
+
+    sandwiches.forEach((sandwich)=>{
+        const section = document.createElement("section");
+        section.classList.add("menu-option");
+        const img = document.createElement("img");
+        img.src=`json/images/${sandwich.img}`;
+        const h3 = document.createElement("h3");
+        h3.innerHTML=sandwich.name;
+        section.append(img);
+        section.append(h3);
+        if(sandwich.hot=="true"){
+            hotmenu.append(section);
+        }
+        else{
+            coldmenu.append(section);
+        }
+        section.onclick=()=>{
+            //popup shite
+        };
+    });
 }
 
 showSandwiches();
-
-//json reading into array
-
-//initializing
